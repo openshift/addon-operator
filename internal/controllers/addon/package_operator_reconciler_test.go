ed37b87c3418756daa5cec20eb6298dc2fdd94b9
Alessandro Costa <alecosta.me@gmail.com>
2023-04-04 18:44:33 +0200

    Improve PackageOperatorReconciler test

--- internal/controllers/addon/package_operator_reconciler_test.go
+++ internal/controllers/addon/package_operator_reconciler_test.go
@@ -20,95 +20,131 @@ import (
 	"github.com/openshift/addon-operator/internal/testutil"
 )
 
-func TestPackageOperatorReconciler_Name(t *testing.T) {
+var (
+	addonWithoutPKO = testutil.NewTestAddonWithSingleNamespace()
+	addonWithPKO    = &addonsv1alpha1.Addon{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-addon", Namespace: "test-ns"},
+		Spec:       addonsv1alpha1.AddonSpec{AddonPackageOperator: &v1alpha1.AddonPackageOperator{Image: "test-pko-img"}},
+	}
+)
+
+type subTest struct {
+	// function that configures mock client for the specific test
+	configureClient func(context.Context, *testutil.Client, types.NamespacedName)
+	// test asserts for Error or NotError depending on this boolean
+	expectError bool
+}
+
+func TestPackageOperatorReconcilerName(t *testing.T) {
 	c := testutil.NewClient()
 	r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
 	require.Equal(t, "packageOperatorReconciler", r.Name())
 }
 
-func TestPackageOperatorReconciler_Exists(t *testing.T) {
-	c := testutil.NewClient()
+func TestPackageOperatorReconcilerLogic(t *testing.T) {
+	for name, test := range map[string]struct {
+		addon    *v1alpha1.Addon
+		subTests map[string]subTest
+	}{
+		"ClusterObjectTemplateMustExist": {
+			addonWithPKO,
+			map[string]subTest{
+				"Found": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(nil).
+							Once()
+						c.On("Patch", ctx, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), mock.Anything, []client.PatchOption(nil)).
+							Return(nil).
+							Once()
+					},
+					false,
+				},
+				"NotFound": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(errors.NewNotFound(schema.GroupResource{}, "test")).
+							Once()
+						c.On("Create", ctx, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.CreateOption(nil)).
+							Return(nil).
+							Once()
+					},
+					false,
+				},
+				"Error": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						errIn := io.ErrClosedPipe
+						c.
+							On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(errors.NewNotFound(schema.GroupResource{}, "test")).
+							Once().
+							Return(errIn).
+							Once()
+					},
+					true,
+				},
+			},
+		},
+		"ClusterObjectTemplateMustNotExist": {
+			addonWithoutPKO,
+			map[string]subTest{
+				"Found": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(nil).
+							Once()
+						c.On("Delete", ctx, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.DeleteOption(nil)).
+							Return(nil).
+							Once()
+					},
+					false,
+				},
+				"NotFound": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(errors.NewNotFound(schema.GroupResource{}, "test")).
+							Once()
+					},
+					false,
+				},
+				"Error": {
+					func(ctx context.Context, c *testutil.Client, identifier types.NamespacedName) {
+						errIn := io.ErrClosedPipe
+						c.
+							On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).
+							Return(errors.NewNotFound(schema.GroupResource{}, "test")).
+							Once().
+							Return(errIn).
+							Once()
+					},
+					true,
+				},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			for subName, subTest := range test.subTests {
+				t.Run(subName, func(t *testing.T) {
+					c := testutil.NewClient()
 
-	image := "quayeiei"
-	namespace := "testnamespace"
-	addonname := "addonname"
-	identifier := types.NamespacedName{Namespace: namespace, Name: addonname}
+					identifier := types.NamespacedName{Namespace: test.addon.Namespace, Name: test.addon.Name}
 
-	a := &addonsv1alpha1.Addon{
-		ObjectMeta: metav1.ObjectMeta{Name: addonname, Namespace: namespace},
-		Spec:       addonsv1alpha1.AddonSpec{AddonPackageOperator: &v1alpha1.AddonPackageOperator{Image: image}},
+					ctx := context.Background()
+					subTest.configureClient(ctx, c, identifier)
+
+					r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
+
+					res, err := r.Reconcile(ctx, test.addon)
+
+					if subTest.expectError {
+						require.Error(t, err)
+					} else {
+						require.NoError(t, err)
+					}
+					require.Equal(t, reconcile.Result{}, res)
+					c.AssertExpectations(t)
+				})
+			}
+		})
 	}
-
-	ctx := context.Background()
-	c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).Return(nil).Once()
-	c.On("Patch", ctx, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), mock.Anything, []client.PatchOption(nil)).Return(nil).Once()
-
-	r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
-
-	res, err := r.Reconcile(ctx, a)
-	require.NoError(t, err)
-	require.Equal(t, reconcile.Result{}, res)
-	c.AssertExpectations(t)
-}
-
-func TestPackageOperatorReconciler_NotFound(t *testing.T) {
-	c := testutil.NewClient()
-
-	image := "quayeiei"
-	namespace := "testnamespace"
-	addonname := "addonname"
-	identifier := types.NamespacedName{Namespace: namespace, Name: addonname}
-
-	a := &addonsv1alpha1.Addon{
-		ObjectMeta: metav1.ObjectMeta{Name: addonname, Namespace: namespace},
-		Spec:       addonsv1alpha1.AddonSpec{AddonPackageOperator: &v1alpha1.AddonPackageOperator{Image: image}},
-	}
-
-	ctx := context.Background()
-	c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).Return(errors.NewNotFound(schema.GroupResource{}, "test")).Once()
-	c.On("Create", ctx, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.CreateOption(nil)).Return(nil).Once()
-
-	r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
-
-	res, err := r.Reconcile(ctx, a)
-	require.NoError(t, err)
-	require.Equal(t, reconcile.Result{}, res)
-	c.AssertExpectations(t)
-}
-
-func TestPackageOperatorReconciler_Err(t *testing.T) {
-	c := testutil.NewClient()
-
-	image := "quayeiei"
-	namespace := "testnamespace"
-	addonname := "addonname"
-	identifier := types.NamespacedName{Namespace: namespace, Name: addonname}
-
-	a := &addonsv1alpha1.Addon{
-		ObjectMeta: metav1.ObjectMeta{Name: addonname, Namespace: namespace},
-		Spec:       addonsv1alpha1.AddonSpec{AddonPackageOperator: &v1alpha1.AddonPackageOperator{Image: image}},
-	}
-
-	errIn := io.ErrClosedPipe
-	ctx := context.Background()
-	c.On("Get", ctx, identifier, mock.AnythingOfType("*v1alpha1.ClusterObjectTemplate"), []client.GetOption(nil)).Return(errors.NewNotFound(schema.GroupResource{}, "test")).Once().Return(errIn).Once()
-
-	r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
-
-	res, err := r.Reconcile(ctx, a)
-	require.Error(t, err)
-	require.Equal(t, reconcile.Result{}, res)
-	c.AssertExpectations(t)
-}
-
-func TestPackageOperatorReconciler_NoPKO(t *testing.T) {
-	c := testutil.NewClient()
-	a := testutil.NewTestAddonWithSingleNamespace()
-	r := &addon.PackageOperatorReconciler{Client: c, Scheme: testutil.NewTestSchemeWithAddonsv1alpha1()}
-
-	ctx := context.Background()
-	result, err := r.Reconcile(ctx, a)
-	require.NoError(t, err)
-	require.Equal(t, reconcile.Result{}, result)
-	c.AssertExpectations(t)
 }
