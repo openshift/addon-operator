c3caa0c95de8f78d96420574c481bb5cd208a785
Supreeth Basabattini <sbasabat@redhat.com>
2023-07-28 19:28:44 +0530

    Add reason to test cases

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -93,6 +93,7 @@ func TestAddonMetrics_AddonHealth(t *testing.T) {
 				{
 					Type:   addonsv1alpha1.Available,
 					Status: metav1.ConditionFalse,
+					Reason: addonsv1alpha1.AddonReasonUnreadyCSV,
 				},
 			}),
 			expected: float64(0),
@@ -102,6 +103,7 @@ func TestAddonMetrics_AddonHealth(t *testing.T) {
 				{
 					Type:   addonsv1alpha1.Available,
 					Status: metav1.ConditionTrue,
+					Reason: addonsv1alpha1.AddonReasonFullyReconciled,
 				},
 			}),
 			expected: float64(1),
@@ -127,10 +129,12 @@ func TestAddonMetrics_AddonHealth(t *testing.T) {
 			// local copy of the addon
 			addon := tc.addon.DeepCopy()
 			addon.Name = fmt.Sprintf("%s-%d", addonNamePrefix, i)
+			addonConditions := addon.Status.Conditions
 
-			healthReason := ""
-			if len(addon.Status.Conditions) == 0 {
-				healthReason = "Unknown"
+			healthReason := "Unknown"
+
+			if len(addonConditions) != 0 {
+				healthReason = addonConditions[0].Reason
 			}
 
 			recorder.recordAddonHealthInfo(addon)
